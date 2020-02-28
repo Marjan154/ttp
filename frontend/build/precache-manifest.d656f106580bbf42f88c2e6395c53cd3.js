0a3d8369fc8919055038e019084860b02d4b97a6
MarjanGit <mansar16@huntersoe.org>
2020-02-28 17:14:15 -0500

    revert

---
+++ frontend/build/precache-manifest.d656f106580bbf42f88c2e6395c53cd3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d42d8e0f563bbcee9907a01e40ff0fac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0506161b6409d8967cfe",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "3c984b8b66fed98ee6f0",
+    "url": "/static/css/main.8df3e116.chunk.css"
+  },
+  {
+    "revision": "0506161b6409d8967cfe",
+    "url": "/static/js/2.9e1028c5.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/static/js/2.9e1028c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c984b8b66fed98ee6f0",
+    "url": "/static/js/main.bc9054d2.chunk.js"
+  },
+  {
+    "revision": "6ea136010a7bde5b66de",
+    "url": "/static/js/runtime-main.c1f2603e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1de19f4e1265726e90820365aef65911",
+    "url": "/static/media/payment-method.1de19f4e.png"
+  },
+  {
+    "revision": "ebbe274aa92a4aef862cb44f85a4178d",
+    "url": "/static/media/transaction.ebbe274a.png"
+  },
+  {
+    "revision": "44803aa41662e5f436eb7171c0ccb8fc",
+    "url": "/static/media/user.44803aa4.png"
+  }
+]);
