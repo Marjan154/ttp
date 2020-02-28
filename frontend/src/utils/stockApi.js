ec5879c518435aa0628e2cd28fe548baa98185c7
MarjanGit <mansar16@huntersoe.org>
2020-02-28 17:37:27 -0500

    back to public key

--- frontend/src/utils/stockApi.js
+++ frontend/src/utils/stockApi.js
@@ -5,7 +5,7 @@ const key = "Tpk_629c2ff3f36c4aa4821c7f061431f683";
 const skey = process.env.REACT_APP_IEX_API_KEY;
 
 export let getAllStocks = async searchval => {
-  let url = `https://cloud.iexapis.com/stable/search/${searchval}?token=${skey}`;
+  let url = `https://sandbox.iexapis.com/stable/search/${searchval}?token=${key}`;
   let stockdata = await axios.get(url).then(data => data.data);
   let symbols = stockdata.map(stock => stock.symbol);
   let priceData = await getStockAllPrices(symbols);
@@ -14,13 +14,13 @@ export let getAllStocks = async searchval => {
 
 export let getStockAllPrices = async symbols => {
   let symbolsS = symbols.join(",");
-  let url = `https://cloud.iexapis.com/stable/stock/market/batch?symbols=${symbolsS}&types=price,quote,ohlc&token=${skey}`;
+  let url = `https://sandbox.iexapis.com/stable/stock/market/batch?symbols=${symbolsS}&types=price,quote,ohlc&token=${key}`;
   let sdata = await axios.get(url).then(data => data.data);
   return sdata;
 };
 
 export let getStockPrice = async symbol => {
-  let url = `https://cloud.iexapis.com/stable/stock/${symbol}/quote/latestPrice?token=${skey}`;
+  let url = `https://sandbox.iexapis.com/stable/stock/${symbol}/quote/latestPrice?token=${key}`;
   let sprice = await axios.get(url).then(data => {
     return data.data;
   });
