7d737788c85639671c7ca8523c577925a1f5b7a2
MarjanGit <mansar16@huntersoe.org>
2020-02-28 12:40:46 -0500

    removed old client and serevr folders

--- server/api/index.js
+++
@@ -1,6 +0,0 @@
-const router = require("express").Router();
-module.exports = router;
-
-router.use("/users", require("./User"));
-router.use("/auth", require("./Auth"));
-router.use("/transactions", require("./Transaction"));
--- server/models/index.js
+++
@@ -1,9 +0,0 @@
-const db = require("./db");
-const Users = require("./Users");
-const Transaction = require("./Transaction");
-
-module.exports = {
-  db,
-  Users,
-  Transaction
-};
